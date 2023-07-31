22cf72fbe0a56624f3536732e5236b2f51315cfa
Светлана Суворова <myrrh@myrrh.ru>
2023-07-31 20:03:01 +0300

    Feat: сверстать раздел О проекте

--- src/components/Main/AboutProject/AboutProject.jsx
+++ src/components/Main/AboutProject/AboutProject.jsx
@@ -4,18 +4,20 @@ function AboutProject() {
     return (
         <section className='about-project'>
             <h2 className='about-project__heading'>О проекте</h2>
-            <article>
-                <h3 className='about-project__title'>Дипломный проект включал 5 этапов</h3>
-                <p className='about-project__paragraph'>Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
-            </article>
-            <article>
-                <h3 className='about-project__title'>На выполнение диплома ушло 5 недель</h3>
-                <p className='about-project__paragraph'>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
-            </article>
+            <div className='about-project__table'>
+                <article>
+                    <h3 className='about-project__table-title'>Дипломный проект включал 5 этапов</h3>
+                    <p className='about-project__table-paragraph'>Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+                </article>
+                <article>
+                    <h3 className='about-project__table-title'>На выполнение диплома ушло 5 недель</h3>
+                    <p className='about-project__table-paragraph'>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+                </article>
+            </div>
             <div className='about-project__process'>
-                <div className='about-project__process-scale'>1 неделя</div>
-                <p className='about-project__process-text'>Back-end</p>
-                <div className='about-project__process-scale'>4 недели</div>
+                <p className='about-project__process-scale about-project__process-scale_green'>1 неделя</p>
+                <p className='about-project__process-scale'>4 недели</p>
+                <p className='about-project__process-text'>Back-end</p>                
                 <p className='about-project__process-text'>Front-end</p>
             </div>
         </section>

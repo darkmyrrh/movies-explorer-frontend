2011f3b849b672b3cc3e87205ed32cd480ca582f
Светлана Суворова <myrrh@myrrh.ru>
2023-08-02 17:02:05 +0300

    Feat: сверстать раздел Портфолио

---
+++ src/components/Main/Portfolio/Portfolio.jsx
@@ -0,0 +1,40 @@
+import "./Portfolio.css";
+
+function Portfolio() {
+  return (
+    <section className="portfolio">
+      <h2 className="portfolio__heading">Портфолио</h2>
+      <ul className="potfolio__links">
+        <a
+          href="https://darkmyrrh.github.io/how-to-learn/"
+          className="portfolio__link"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <p className="portfolio__link-text">Статичный сайт</p>
+          <p className="portfolio__link-text">&#129125;</p>
+        </a>
+        <a
+          href="https://darkmyrrh.github.io/russian-travel/"
+          className="portfolio__link"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <p className="portfolio__link-text">Адаптивный сайт</p>
+          <p className="portfolio__link-text">&#129125;</p>
+        </a>
+        <a
+          href="https://mesto.myrrh.ru"
+          className="portfolio__link"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <p className="portfolio__link-text">Одностраничное приложение</p>
+          <p className="portfolio__link-text">&#129125;</p>
+        </a>
+      </ul>
+    </section>
+  );
+}
+
+export default Portfolio;
